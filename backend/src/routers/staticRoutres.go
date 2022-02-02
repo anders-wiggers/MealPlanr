3571440cd6e2d5b65e08d05cc61853893c209074
anders-wiggers <contact@anrs.dk>
2022-02-02 01:38:43 +0100

    fixed routing bug

--- backend/src/routers/staticRoutres.go
+++ backend/src/routers/staticRoutres.go
@@ -2,10 +2,23 @@ package routers
 
 //Static Routes all versions of the API should adhere to
 type StaticRoutes struct {
-	UserRoute string 			`default:"/user"`
-	RecipesRoute string			`deafult:"/user/recipes"`
-	RecipeRoute string			`deafult:"/user/recipe"`
-	PlannerRoute string			`deafult:"/planner"`
-	ShoppingListRoute string 	`deafult:"/planner/shoppinglist"`
-	LoginRoute string 			`deafult:"/login"`
+	UserRoute         string `default:"/user"`
+	RecipesRoute      string `deafult:"/user/recipes"`
+	RecipeRoute       string `deafult:"/user/recipe"`
+	PlannerRoute      string `deafult:"/planner"`
+	ShoppingListRoute string `deafult:"/planner/shoppinglist"`
+	LoginRoute        string `deafult:"/login"`
+}
+
+func NewStaticRoutes() *StaticRoutes {
+	router := StaticRoutes{}
+
+	router.UserRoute = "/user"
+	router.RecipesRoute = "/user/recipes"
+	router.RecipeRoute = "/user/recipe"
+	router.PlannerRoute = "/planner"
+	router.ShoppingListRoute = "/planner/shoppinglist"
+	router.LoginRoute = "/login"
+
+	return &router
 }
--- backend/src/routers/v1/index.go
+++ backend/src/routers/v1/index.go
@@ -8,32 +8,31 @@ import (
 )
 
 //Initiate Routes for Version 1 of the API
-func InitRoutes(g *gin.RouterGroup){
+func InitRoutes(g *gin.RouterGroup) {
 	// Default test route
-	g.GET("/",controllerV1.DefaultResponse)
+	g.GET("/", controllerV1.DefaultResponse)
 
-	routes := &routers.StaticRoutes{}
+	routes := routers.NewStaticRoutes()
 
 	// User routes
-	g.GET(routes.UserRoute,controllerV1.DefaultResponse)
-	g.POST(routes.UserRoute,controllerV1.DefaultResponse)
-	g.PATCH(routes.UserRoute,controllerV1.DefaultResponse)
+	g.GET(routes.UserRoute, controllerV1.DefaultResponse)
+	g.POST(routes.UserRoute, controllerV1.DefaultResponse)
+	g.PATCH(routes.UserRoute, controllerV1.DefaultResponse)
 
 	// User recipes routes
-	g.GET(routes.RecipesRoute,controllerV1.DefaultResponse)
-	g.GET(routes.RecipeRoute,controllerV1.DefaultResponse)
-	g.POST(routes.RecipeRoute,controllerV1.DefaultResponse)
-	g.DELETE(routes.RecipeRoute,controllerV1.DefaultResponse)
+	g.GET(routes.RecipesRoute, controllerV1.DefaultResponse)
+	g.GET(routes.RecipeRoute, controllerV1.DefaultResponse)
+	g.POST(routes.RecipeRoute, controllerV1.DefaultResponse)
+	g.DELETE(routes.RecipeRoute, controllerV1.DefaultResponse)
 
-	// Planner Routres 
-	g.GET(routes.PlannerRoute,controllerV1.DefaultResponse)
-	g.POST(routes.PlannerRoute,controllerV1.DefaultResponse)
+	// Planner Routres
+	g.GET(routes.PlannerRoute, controllerV1.DefaultResponse)
+	g.POST(routes.PlannerRoute, controllerV1.DefaultResponse)
 
 	// ShoppingList routes
-	g.GET(routes.ShoppingListRoute,controllerV1.DefaultResponse)
-	g.PATCH(routes.ShoppingListRoute,controllerV1.DefaultResponse)
+	g.GET(routes.ShoppingListRoute, controllerV1.DefaultResponse)
+	g.PATCH(routes.ShoppingListRoute, controllerV1.DefaultResponse)
 
 	// Login Routes
-	g.POST(routes.LoginRoute,controllerV1.DefaultResponse)
+	g.POST(routes.LoginRoute, controllerV1.DefaultResponse)
 }
-
